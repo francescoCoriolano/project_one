193e729b9bd3b53e81150d7597abc98b906136ae
Francesco Coriolano <francescocoriolano@Francescos-MBP.home>
2022-07-22 17:52:19 +0100

    starting the header

---
+++ src/Components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./style.scss"
+const Header = () => {
+  return (
+    <div className='header'>
+<button>New!</button>
+<h1>All Natural Skin Remedies. </h1>
+<p>A healthier you from the inside out. We've sourced the cleanest ingredientes to create a line of clean skin care treatments that leave you feeling your best</p>
+<button>Shop products</button>
+    </div>
+  )
+}
+
+export default Header
