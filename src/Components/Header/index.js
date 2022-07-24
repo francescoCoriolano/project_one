79521b625010281415577cae4b358e2909f33f36
Francesco Coriolano <francescocoriolano@Francescos-MBP.home>
2022-07-24 12:56:30 +0100

    adjust text

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import "./style.scss"
+
 const Header = () => {
   return (
     <div className='header'>
-<button>New!</button>
-<h1>All Natural Skin Remedies. </h1>
-<p>A healthier you from the inside out. We've sourced the cleanest ingredientes to create a line of clean skin care treatments that leave you feeling your best</p>
-<button>Shop products</button>
+      <div className='header__text__container'>
+        <div className='text__wrapper'>
+          <div className='new'>New!</div>
+          <h1 className='header__title'>All Natural <br></br> Skin Remedies. </h1>
+          <p className='header__text'>A healthier you from the inside out. We've sourced the <br></br> cleanest ingredientes to create a line of clean skin care treatments that leave you feeling your best</p>
+          <button className='shop__btn'>Shop products</button>
+        </div>
+
+      </div>
+      <div className='header__img__container'></div>
+
+
+
+
     </div>
   )
 }
