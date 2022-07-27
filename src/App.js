d9875313fc6449ea7f9d68ed4dfb6475828a1726
Francesco Coriolano <francescocoriolano@Francescos-MBP.home>
2022-07-27 12:31:15 +0100

    create login route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import './App.scss';
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from './components/Login';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import ProductsOverview from './components/ProductsOverview';
 import ShopSection from './components/ShopSection';
 
 function App() {
+
+  const root = ReactDOM.createRoot(
+    document.getElementById("root")
+  );
+
   return (
-    <div className="App">
+    <>
+       <BrowserRouter>
+          <Routes>
+            <Route path='/login' element={<Login/>}/>
+          </Routes>
+        </BrowserRouter>
+      <div className="App"> 
       <Navbar/>
       <Header/>
       <ProductsOverview/>
       <ShopSection/>
-    </div>
+      </div> 
+      </>
   );
 }
 
