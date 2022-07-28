8d3e7bfdc1542141d7afb8a76626bc6730c4c3a8
Francesco Coriolano <francescocoriolano@Francescos-MacBook-Pro.local>
2022-07-28 10:33:23 +0100

    removed comments and scroll bar on shop overview

--- src/Components/ShopSection/index.js
+++ src/Components/ShopSection/index.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import './style.scss'
-import flowerFace from '../../assets/images/flower-face.png'
+import React from "react";
+import "./style.scss";
+import flowerFace from "../../assets/images/flower-face.png";
 const ShopSection = () => {
   return (
-    <div className='shop-section'>
-        <div className='text-container'>
-        <div className='text-wrapper'>
-          <h2 className='header-title'>Clean products that <br></br>deliver better solutions</h2>
-          <p className='header-text'>A healthier you from the inside out. We've sourced the <br></br> cleanest ingredientes to create a line of clean skin care <br></br> treatments that leave you feeling your best</p>
-          <button className='shop-btn' >Shop more</button>
+    <div className="shop-section">
+      <div className="text-container">
+        <div className="text-wrapper">
+          <h2 className="header-title">
+            Clean products that <br></br>deliver better solutions
+          </h2>
+          <p className="header-text">
+            A healthier you from the inside out. We've sourced the <br></br>{" "}
+            cleanest ingredientes to create a line of clean skin care <br></br>{" "}
+            treatments that leave you feeling your best
+          </p>
+          <button className="shop-btn">Shop more</button>
         </div>
-        </div>
-        <div className='img-container'>
-            <img src={flowerFace} alt='flower-face'/>
-        </div>
+      </div>
+      <div className="img-container"></div>
     </div>
-  )
-}
+  );
+};
 
-export default ShopSection
+export default ShopSection;
