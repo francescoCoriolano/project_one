89e5c1a6e2a998403972b49a6e6f3a81b6e84649
Francesco Coriolano <francescocoriolano@Francescos-MBP.home>
2022-08-01 22:08:03 +0100

    price is showing but discount is not apply yet

--- src/Components/CardItem/index.js
+++ src/Components/CardItem/index.js
@@ -3,48 +3,56 @@ import "./style.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
-const CardItem = ({
-  image,
-  name,
-  description,
-  price,
-  category,
-  mostRated,
-  index,
-}) => {
-  // const applyDiscount = (mostRated) => {
-  //   for (let i = 0; i < mostRated.length; i++) {
-  //     if (mostRated[i].title.includes("Women")) {
-  //       const price = mostRated[i].price;
+const CardItem = ({ image, name, description, price, category, index }) => {
+  const localStorageData = JSON.parse(localStorage.getItem("productsData"));
+  console.log("lcd", localStorageData);
 
-  //       return price;
-  //     }
-  //   }
-  //   const newPrice = price - price * 0.15;
-  //   return newPrice;
-  // };
-  // // best way to resolve this warning?
-  // useEffect(() => {
-  //   applyDiscount(mostRated);
-  // }, []);
+  const mostRated = localStorageData
+    .sort((a, b) => b.rating.rate - a.rating.rate)
+    .splice(0, 4);
+  console.log("mr", mostRated);
 
+  const applyDiscount = (mostRated) => {
+    for (let i = 0; i < mostRated.length; i++) {
+      if (mostRated[i].title.includes("Women")) {
+        const price = mostRated[i].price;
+        const isWoman = mostRated[i].title.includes("Women");
+
+        return { price, isWoman };
+      }
+    }
+
+    const newPrice = price - price * 0.15;
+    return { newPrice };
+  };
+
+  // best way to resolve this warning?
+  useEffect(() => {
+    applyDiscount(mostRated);
+  }, []);
+
+  //this was not working here so I move it inside the applyDiscount function
   // const isWoman = mostRated[index].title.includes("Women");
 
-  // console.log("mostrated", mostRated);
+  console.log("mostrated", mostRated);
 
   return (
     <div>
-      {/* <div className="card-item">
+      <div className="card-item">
         <div className="card-img">
           {category === "jewelery" && <div className="new-label">New!</div>}
-          {isWoman ? <div className=" new-label discount">15%</div> : ""}
+          {applyDiscount.isWoman ? (
+            <div className=" new-label discount">15%</div>
+          ) : (
+            ""
+          )}
           <img src={image} alt="product-img" />
         </div>
         <div className="item-description-wrapper">
           <h3 className="item-name">{name}</h3>
           <p className="item-description">{description}</p>
           <div className="price-wrapper">
-            {isWoman ? (
+            {applyDiscount.isWoman ? (
               <span className="discount item-price">
                 ${applyDiscount(mostRated[index].price).toFixed(2)}
               </span>
@@ -52,7 +60,9 @@ const CardItem = ({
               ""
             )}
             <span
-              className={`item-price ${isWoman ? "item-price-reduced" : ""} `}
+              className={`item-price ${
+                applyDiscount.isWoman ? "item-price-reduced" : ""
+              } `}
             >
               ${price}
             </span>
@@ -63,8 +73,8 @@ const CardItem = ({
             <span>Add to cart</span>
           </button>
         </div>
-      </div> */}
-      <div className="card-item">
+      </div>
+      {/* <div className="card-item">
         <div className="card-img">
           {category === "jewelery" && <div className="new-label">New!</div>}
 
@@ -80,7 +90,7 @@ const CardItem = ({
             <span>Add to cart</span>
           </button>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
