72225bc2401298b88f04ebf1dab50b0ae7d94c0d
Francesco Coriolano <francescocoriolano@Francescos-MBP.home>
2022-08-17 12:00:53 +0100

    removed console.log

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
   const addItemToCart = (id) => {
     setCart([...cart, id]);
     localStorage.setItem("cartData", JSON.stringify([...cart, id]));
-    console.log("cart", cart);
   };
 
   useEffect(() => {
