b4c9fd1aaf1d63dc247678f64a621ce90064a30b
Francesco Coriolano <francescocoriolano@Francescos-MBP.home>
2022-08-23 11:18:27 +0100

    removed come dead code

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -8,15 +8,14 @@ export const CartProvider = ({ children }) => {
   const addToCart = (id) => {
     setItemsCart([...itemsCart, id]);
     localStorage.setItem("itemsCart", JSON.stringify([...itemsCart, id]));
-    console.log("cart", itemsCart);
   };
+
   useEffect(() => {
     const cartItemsLocal = JSON.parse(localStorage.getItem("itemsCart"));
     if (cartItemsLocal) {
       setItemsCart(cartItemsLocal);
-      console.log("jjjj", itemsCart);
     }
-    console.log("localStorageItemss", localStorage);
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
